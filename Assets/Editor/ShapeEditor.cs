9e87df90845ace59d08ba5c9676716e1812af8d8
Redx <17arman08@gmail.com>
2017-10-03 19:52:35 +0200

    Initial commit

---
+++ Assets/Editor/ShapeEditor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ShapeCreator))]
+public class ShapeEditor : Editor {
+    private ShapeCreator shapeCreator;
+    private bool needsRepaint;
+
+    private void OnSceneGUI() {
+        Event guiEvent = Event.current;
+
+        if (guiEvent.type == EventType.Repaint) {
+            Draw();
+        } else if (guiEvent.type == EventType.layout) {
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+        } else {
+            HandleInput(guiEvent);
+            if (needsRepaint) {
+                HandleUtility.Repaint();
+            }
+        }
+    }
+
+    private void HandleInput(Event guiEvent) {
+        Ray mouseRay = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition);
+        float drawPlaneHeight = 0;
+        float dstToDrawPlane = (drawPlaneHeight - mouseRay.origin.y) / mouseRay.direction.y;
+        Vector3 mousePosition = mouseRay.GetPoint(dstToDrawPlane);
+
+        if (guiEvent.type == EventType.mouseDown && guiEvent.button == 0 && guiEvent.modifiers == EventModifiers.None) {
+            Undo.RecordObject(shapeCreator, "Add point");
+            shapeCreator.points.Add(mousePosition);
+            needsRepaint = true;
+        }
+    }
+
+    private void Draw() {
+        for (int i = 0; i < shapeCreator.points.Count; i++) {
+            Vector3 nextPoint = shapeCreator.points[(i + 1) % shapeCreator.points.Count];
+            Handles.color = Color.black;
+            Handles.DrawDottedLine(shapeCreator.points[i], nextPoint, 4f);
+            Handles.color = Color.white;
+            Handles.DrawSolidDisc(shapeCreator.points[i], Vector3.up, 0.5f);
+        }
+        needsRepaint = false;
+    }
+
+    private void OnEnable() {
+        shapeCreator = target as ShapeCreator;
+    }
+}
---
+++ Assets/ShapeCreator.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeCreator : MonoBehaviour {
+    [HideInInspector] public List<Vector3> points = new List<Vector3>();
+}
